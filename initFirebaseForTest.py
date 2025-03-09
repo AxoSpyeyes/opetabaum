73ab31ef02e3be3d42466bc463004d34f550b167
axodaddy <73425961+axodaddy@users.noreply.github.com>
2025-03-09 16:54:20 +0000

    Cleaned up code

--- initFirebaseForTest.py
+++ initFirebaseForTest.py
@@ -2,22 +2,21 @@ from suha import Kotoli
 from mellan import format_fras  
 
 from firestore_db import Database
-db2 = Database()
-db = db2.db # hack to not break old code
+db = Database()
 
 kotoli = Kotoli()
 
 def reset_ko():
-    print(db2.delete_all_docs("ko"))
-    print (db2.batch_insert_docs("ko", kotoli.kirain_kaban["ko"]))
+    print(db.delete_all_docs("ko"))
+    print (db.batch_insert_docs("ko", kotoli.kirain_kaban["ko"]))
 
 def reset_ttb():
-    print(db2.delete_all_docs("ttb"))
-    print (db2.batch_insert_docs("ttb", kotoli.kirain_kaban["ttb"]))
+    print(db.delete_all_docs("ttb"))
+    print (db.batch_insert_docs("ttb", kotoli.kirain_kaban["ttb"]))
 
 def reset_brukdjin():
-    print(db2.delete_all_docs("brukdjin"))
-    print (db2.batch_insert_docs("brukdjin", kotoli.kirain_kaban["brukdjin"]))
+    print(db.delete_all_docs("brukdjin"))
+    print (db.batch_insert_docs("brukdjin", kotoli.kirain_kaban["brukdjin"]))
 
 def reset_everything():
     reset_ko()
